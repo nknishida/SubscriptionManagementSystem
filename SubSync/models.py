ae9e756908ea665084f6b9ddaa32fd2ffaca68af
nknishida <nknishidasheri@gmail.com>
2025-04-01 14:11:23 +0530

    Add blank line for improved readability in Resource model

--- SubSync/models.py
+++ SubSync/models.py
@@ -766,6 +766,7 @@ class Resource(models.Model):
 
     hosting_type = models.CharField(max_length=100, blank=True, null=True)
     # hosting_location_name= models.CharField(max_length=100, blank=True, null=True)
+    
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
